cb84272aeec023a3bcfc63d7a587fe3c51f94b98
Teppei Shintani <teppest1215@gmail.com>
2023-11-25 11:04:55 +0900

    fix

--- scripts/link_etc.sh
+++ scripts/link_etc.sh
@@ -5,13 +5,10 @@ set -eu
 MYSQL_CONF_DIR=/etc/mysql
 NGINX_CONF_DIR=/etc/nginx
 
-mkdir -p $HOME/$MYSQL_CONF_DIR
 mkdir -p $HOME/$NGINX_CONF_DIR
 
 TARGETS=(
         /etc/sysctl.conf
-        /etc/mysql/my.cnf
-        /etc/mysql/conf.d
         /etc/nginx/nginx.conf
         /etc/nginx/conf.d
         /etc/nginx/sites-enabled
